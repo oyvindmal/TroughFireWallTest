405cf7462a0e6492bc8064523aee0c9af4fd57c3
Øyvind Malin <oyvind@epostboks.org>
2014-06-02 21:06:42 -0400

    lol

--- client.js
+++ client.js
@@ -12,6 +12,7 @@ client.on('data', function(data) {
         console.log('Received: ' + data);
         if(data === "something")
         {
+            console.log("Doing something");
             exec("tdtool -n 4", null)
         }
 //      client.destroy(); // kill client after server's response
