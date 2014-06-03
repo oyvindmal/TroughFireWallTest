b0a11a57cafd878cccf91af716ddb45a9ae5d87e
Øyvind Malin <oyvind@epostboks.org>
2014-06-02 20:54:04 -0400

    lol

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ var server = net.createServer(function (socket) {
 });
 
 server.listen(5000);
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+  
+  // socket.write("something");
+}).listen(1337);
