331ab7e50aba96f2883e5da75f03d71c5e911ed9
Øyvind Malin <oyvind@epostboks.org>
2014-06-02 20:59:29 -0400

    fixed

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var net = require('net');
 
+var http = require('http');
+
 var server = net.createServer(function (socket) {
   socket.write('Echo server\r\n');
+  http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+  
+  socket.write("something");
+}).listen(80);
+
   socket.pipe(socket);
 });
 
 server.listen(5000);
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  
-  // socket.write("something");
-}).listen(1337);
