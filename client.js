23f4917e994ec9ba62aad4cd63c19098020d24eb
Øyvind Malin <oyvind@epostboks.org>
2014-06-02 21:39:08 -0400

    params

--- client.js
+++ client.js
@@ -10,11 +10,7 @@ client.connect(5000, 'dhgw.cloudapp.net', function() {
 
 client.on('data', function(data) {
         console.log('Received: ' + data);
-      /*  if(data == "something")
-        {
-            console.log("Doing something");
-            exec("tdtool -n 4", null)
-        }*/
+        console.log(data.split("/"));
 //      client.destroy(); // kill client after server's response
 });
 
